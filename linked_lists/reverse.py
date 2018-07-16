6e7cf58425f3c7bbe25b6334f63d41a7d785d9a4
Domingo Hernandez <embracethemartian03@gmail.com>
2018-07-16 16:32:29 -0500

    Reverse linked list

--- linked_lists/reverse.py
+++ linked_lists/reverse.py
@@ -1,61 +1,31 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-    def __str__(self):
-        # special 'to string' method
-        # defines what should be returned to print() calls
-        # on class instances
-        return str(self.data)
-
-
-def list_str(head):
-    result = ''
-    temp = head
-    while temp:
-        result += str(temp)
-        temp = temp.next
-        if temp:
-            result += ' -> '
-
-    if not result:
-        return 'x'
-
-    return result
+from utils import Node, list_str
 
 
 def reverse(head):
-    """
-    Reverse a linked list iteratively
-    """
-    # add code here
-    # you'll need 3 node variables for previous node, current node, and next_node
-    # iterate through your list
-    #   - then we can do the reversal
-    # the final node set to prev is the new head
-    # return your new head
-    prev = None
     current = head
-    while current:
-        next = current.next
-        current.next = prev
-        prev = current
-        current = next
-    head = prev
+    helper = head
+
+    helper = helper.next
+    helper.next = current
+    current.next = None
+    head = helper
+    
     return head
-    # the three variables was messing me up a bit
 
-if __name__ == '__main__':
-    head = Node(1)
-    second = Node(2)
-    third = Node(3)
-    forth = Node(4)
 
-    head.next = second
-    second.next = third
-    third.next = forth
 
-    result = reverse(head)
+def main():
+    data = [1, 2]#, 3, 4, 5, 6, 7, 8]
+    nodes = list(map(Node, data))
+    for i in range(len(nodes) - 1):
+        nodes[i].next = nodes[i+1]
+    head = nodes[0]
+    print(list_str(head))
 
-    print(list_str(result))
+    new_head = reverse(head)
+
+    print('New head:', new_head)
+    print(list_str(new_head))
+    
+if __name__ == "__main__":
+    main()
