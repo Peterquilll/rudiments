5b2bfd9ff6dbfabeadd9ae2891b437d21e71cbff
Raul Mireles <rmireles@alienvault.com>
2018-07-19 17:32:07 -0500

    Initialize function to find an item in a binary search tree.

---
+++ trees/bst_find.py
@@ -0,0 +1,15 @@
+from binarytree import build, bst
+
+
+def has_element(root, element):
+    return False
+
+
+def main():
+    bst_root = bst(height=2)
+
+    print('bst: ', bst_root)
+
+
+if __name__ == '__main__':
+    main()
