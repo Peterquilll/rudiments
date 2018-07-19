db9c34aa9d0d2f9dd52cd0d7b2a4433d8d69d0d3
Domingo Hernandez <embracethemartian03@gmail.com>
2018-07-19 18:14:21 -0500

    searches for n in a binary tree

--- trees/bst_find.py
+++ trees/bst_find.py
@@ -1,15 +1,29 @@
 from binarytree import build, bst
 
 
-def has_element(root, element):
+def has_element(root, k):
+    if root is None:
+        return False
+    if root.value == k:
+        return True
+    if root.left is None and root.right is None:
+        return False
+
+    if k <= root.value:
+        return has_element(root.left, k)
+    if k > root.value:
+        return has_element(root.right, k)
+
     return False
 
 
 def main():
     bst_root = bst(height=2)
-
+    k = 6
     print('bst: ', bst_root)
 
-
+    result = has_element(bst_root, k)
+    print(result)
+    
 if __name__ == '__main__':
     main()
