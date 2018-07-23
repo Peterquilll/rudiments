d6e5836d0e70fd292075fb47153f128192acf992
Raul Mireles <rmireles@alienvault.com>
2018-07-23 16:39:06 -0500

    Initialize bst search with path function.

---
+++ trees/bst_find_with_path.py
@@ -0,0 +1,29 @@
+from binarytree import build, bst
+
+
+def has_element(root, k):
+    if root is None:
+        return False
+    if root.value == k:
+        return True
+    if root.left is None and root.right is None:
+        return False
+
+    if k <= root.value:
+        return has_element(root.left, k)
+
+    return has_element(root.right, k)
+
+
+
+def main():
+    bst_root = bst(height=2)
+    k = 6
+    print('k: ', k)
+    print('bst: ', bst_root)
+
+    result = has_element(bst_root, k)
+    print(result)
+    
+if __name__ == '__main__':
+    main()
